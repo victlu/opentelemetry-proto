fc150e39f1c4e23bc360fbabad5532750b3bc411
victlu <victlu@microsoft.com>
2021-05-27 16:56:55 -0700

    framing

---
+++ tests/dotnet/Benchmark/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Environments;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+
+namespace Benchmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+            => BenchmarkSwitcher
+                .FromAssembly(typeof(Program).Assembly)
+                .Run(args);
+    }
+}
